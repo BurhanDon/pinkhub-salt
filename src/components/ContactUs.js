aef25da8c9b4397d0b59ee1797f2d2235aa3d3ae
BurhanDon <bu61796@gmail.com>
2026-01-07 23:16:22 +0500

    c v2

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -146,6 +146,7 @@ export default function ContactUs() {
             transition={{ duration: 0.7 }}
           >
             <form
+              netlify
               name="contact"
               method="post"
               data-netlify="true"
